39d3e547b36a78a6f11632b3d0677851159b7bfd
xuan <1906574057@qq.com>
2021-07-25 12:19:20 +0800

    练习4：海龟作图：画一个五角星

---
+++ 4/main.py
@@ -0,0 +1,15 @@
+"""
+    练习4
+"""
+
+import turtle
+
+t = turtle.Turtle(shape='turtle')
+
+for i in range(5):
+    t.forward(100)
+    t.right(144)
+    t.forward(100)
+    t.left(72)
+
+turtle.mainloop()
