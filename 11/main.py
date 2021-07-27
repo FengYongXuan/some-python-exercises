da26d8adc90b07a7c4c1f3e3154ca9824945b62a
xuan <1906574057@qq.com>
2021-07-27 12:24:16 +0800

    练习11：双色球随机选注程序

---
+++ 11/main.py
@@ -0,0 +1,30 @@
+"""
+    练习11
+"""
+
+import random
+
+num = int(input('需要多少个随机注：'))
+
+while num:
+    red_balls = set()
+    while len(red_balls) < 6:
+        red_ball = random.randint(1, 33)
+        red_balls.add(red_ball)
+
+    blue_ball = random.randint(1, 16)
+
+    try:
+        flag = int(input("直接输出到屏幕上还是存到文件中？直接输出按'1',存文件按'2': "))
+    except ValueError:
+        print("请输入数字！")
+    else:
+        if flag == 1:
+            print("红色球:", red_balls, "\n蓝色球:", blue_ball)
+        elif flag == 2:
+            with open('double_color_ball.txt', 'a', encoding='utf-8') as targetfile:
+                targetfile.write("\n红色球:" + str(red_balls) + "\n蓝色球:" + str(blue_ball))
+        else:
+            print("请输入1或2！")
+
+        num = num - 1
